21532389871e7dd849071e6fd38ab29b6755131b
vauxhall <zero.griffin@gmail.com>
2020-09-04 11:21:29 +0200

    Initial Commit

---
+++ Oink/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Oink
+//
+//  Created by vaux on 04.09.20.
+//  Copyright © 2020 oink.app. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
