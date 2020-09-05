117b52744b8870d8d1458eef2d27ed6406e57ff4
vauxhall <zero.griffin@gmail.com>
2020-09-05 15:40:48 +0200

    Update scrolls

--- Grid.swift
+++ Grid.swift
@@ -4,13 +4,15 @@ import Combine
 final class Grid: NSScrollView {
     private var subs = Set<AnyCancellable>()
     private var cells = [Cell]()
+    private var positions = [CGPoint]()
     private var cellsHidden = [Int]()
+    private var cellsVisible = [Int]()
     private let items: [Int]
     private let size = CGSize(width: 100, height: 100)
     
     required init?(coder: NSCoder) { nil }
     init() {
-        items = (0 ... 500).map { $0 }
+        items = (0 ... 50).map { $0 }
         
         super.init(frame: .zero)
         translatesAutoresizingMaskIntoConstraints = false
@@ -18,15 +20,53 @@ final class Grid: NSScrollView {
         hasVerticalScroller = true
         contentView.postsBoundsChangedNotifications = true
         
-        NotificationCenter.default.publisher(for: NSView.boundsDidChangeNotification, object: contentView)
-            .sink {
-                print(($0.object as! NSClipView).bounds.minY)
+        NotificationCenter.default.publisher(for: NSView.boundsDidChangeNotification, object: contentView).sink { [weak self] _ in
+//            self?.refresh()
+            
+//            print(($0.object as! NSClipView).bounds.minY)
         }.store(in: &subs)
+    }
+    
+    override var frame: NSRect {
+        didSet {
+            updatePositions()
+            refresh()
+        }
+    }
+    
+    private func updatePositions() {
+        var positions = [CGPoint]()
+        var current = CGPoint(x: -size.width, y: 0)
+        (0 ..< items.count).forEach { _ in
+            current.x += size.width
+            if current.x + size.width > bounds.width {
+                current = .init(x: 0, y: current.y + size.height)
+            }
+            positions.append(current)
+        }
+        self.positions = positions
+    }
+    
+    private var visibles: [Int] {
+        let min = contentView.bounds.minY - size.height
+        let max = contentView.bounds.maxY + 1
+        return (0 ..< items.count).filter {
+            positions[$0].y > min && positions[$0].y < max
+        }
+    }
+    
+    private func refresh() {
+        purgeVisible()
         
-        items.forEach { _ in
+        visibles.forEach {
             let cell = Cell(size)
+            cell.frame.origin = positions[$0]
             documentView!.addSubview(cell)
         }
+    }
+    
+    private func purgeVisible() {
+        
     }
 }
 
