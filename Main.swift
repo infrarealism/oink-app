ae021fc2210c8263188b44c8b3745a2813118f20
vauxhall <zero.griffin@gmail.com>
2020-09-07 09:54:00 +0200

    Clean main

--- Main.swift
+++ Main.swift
@@ -17,51 +17,23 @@ final class Main: NSView {
         grid.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
         grid.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
         grid.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
-        
-        /*
-        let scroll = Scroll()
-        addSubview(scroll)
-        
-        let image = NSImageView()
-        image.translatesAutoresizingMaskIntoConstraints = false
-        image.imageScaling = .scaleProportionallyUpOrDown
-        addSubview(image)
-        
-        let noCache = [kCGImageSourceShouldCache : false] as CFDictionary
-        
-        url.map {
-            FileManager.default.enumerator(at: $0, includingPropertiesForKeys: nil, options: [.producesRelativePathURLs, .skipsHiddenFiles, .skipsPackageDescendants])?.forEach {
-                guard
-                    let url = $0 as? URL,
-                    NSImage.imageTypes.contains(url.mime)
-                else { return }
+ 
+        FileManager.default.enumerator(at: url!, includingPropertiesForKeys: nil, options:
+            [.producesRelativePathURLs, .skipsHiddenFiles, .skipsPackageDescendants])?.forEach {
+            guard
+                let url = $0 as? URL,
+                NSImage.imageTypes.contains(url.mime)
+            else { return }
+            
+            let source = try! CGImageSourceCreateWithURL(url as CFURL, [kCGImageSourceShouldCache : false] as CFDictionary)
+            print((CGImageSourceCopyPropertiesAtIndex(source!, 0, nil) as? [String: AnyObject])?["{Exif}"])
                 
-                let source = try! CGImageSourceCreateWithURL(url as CFURL, nil)
-                print(CGImageSourceCopyPropertiesAtIndex(source!, 0, nil) as? [String: AnyObject])
-//                print("-----------------------------------------------")
-                
-                if image.image == nil {
-                    
-                    let x = try! NSBitmapImageRep(data: Data(contentsOf: url))
-//                    print(x)
-                    
-//                    image.image = try! NSImage(data: Data(contentsOf: url))
-                    image.image = NSImage(cgImage: CGImageSourceCreateThumbnailAtIndex(source!, 0, [
-                    kCGImageSourceCreateThumbnailFromImageAlways : false,
-                    kCGImageSourceCreateThumbnailFromImageIfAbsent : false,
-                    kCGImageSourceThumbnailMaxPixelSize : 900] as CFDictionary)!, size: .init(width: 900, height: 900))
-//                    image.image = NSImage(cgImage: CGImageSourceCreateImageAtIndex(source!, 0, nil)!, size: .init(width: 50, height: 50))
-//                    print(raw?.representations)
-                    
-                }
-            }
+                let image = NSImage(cgImage: CGImageSourceCreateThumbnailAtIndex(source!, 0, [
+                kCGImageSourceCreateThumbnailFromImageAlways : false,
+                kCGImageSourceCreateThumbnailFromImageIfAbsent : false,
+                kCGImageSourceThumbnailMaxPixelSize : 900] as CFDictionary)!, size: .init(width: 900, height: 900))
+             
         }
-        
-        image.topAnchor.constraint(equalTo: topAnchor).isActive = true
-        image.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
-        image.widthAnchor.constraint(equalToConstant: 400).isActive = true
-        image.heightAnchor.constraint(equalToConstant: 400).isActive = true
-        */
     }
     
     override func viewDidEndLiveResize() {
---
+++ Photo.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct Photo {
+    let url: URL
+}
